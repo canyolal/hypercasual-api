0a1347d0994c56d7756cf4ef9910fef26e9638ab
Can Yolal <canyolal16@gmail.com>
2022-11-11 20:05:00 +0300

    games listing with filters

--- cmd/api/games.go
+++ cmd/api/games.go
@@ -2,17 +2,50 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/canyolal/hypercasual-inventories/internal/data"
+	"github.com/canyolal/hypercasual-inventories/internal/validator"
 )
 
 func (app *application) listGameHandler(w http.ResponseWriter, r *http.Request) {
 
-	games, _, err := app.models.Game.GetAll()
+	var input struct {
+		Name  string
+		Genre string
+		data.Filters
+	}
+
+	v := validator.New()
+
+	qs := r.URL.Query()
+
+	input.Name = app.readString(qs, "name", "")
+	input.Genre = app.readString(qs, "genre", "")
+
+	input.Filters.Page = app.readInt(qs, "page", 1, v)
+	input.Filters.PageSize = app.readInt(qs, "page_size", 20, v)
+
+	input.Filters.Sort = app.readString(qs, "sort", "id")
+
+	// Allowed sort options.
+	input.Filters.SortSafeList = []string{
+		"id", "name", "genre", "publisher_id",
+		"-id", "-name", "-genre", "-publisher_id",
+	}
+
+	// Validate filter entries
+	if data.ValidateFilters(v, &input.Filters); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	games, _, metadata, err := app.models.Game.GetAllWithFilters(input.Name, input.Genre, input.Filters)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 		return
 	}
 
-	err = app.writeJSON(w, http.StatusOK, envelope{"publishers": games}, nil)
+	err = app.writeJSON(w, http.StatusOK, envelope{"metadata": metadata, "publishers": games}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 	}
--- internal/data/game.go
+++ internal/data/game.go
@@ -3,6 +3,7 @@ package data
 import (
 	"context"
 	"database/sql"
+	"fmt"
 	"time"
 )
 
@@ -31,6 +32,55 @@ func (m *GameModel) Insert(publisherID int64, name, genre string) error {
 	var game Game
 
 	return m.DB.QueryRowContext(ctx, query, name, genre, publisherID).Scan(&game.Id)
+}
+
+// Get all games from db
+func (m *GameModel) GetAllWithFilters(name, genre string, filters Filters) ([]*Game, map[string]string, Metadata, error) {
+	query := fmt.Sprintf(`
+		SELECT count(*) OVER(), id, name, genre, publisher_id, version
+		FROM games
+		WHERE (to_tsvector('simple', name) @@ plainto_tsquery('simple', $1) OR $1 = '')
+		AND (to_tsvector('simple', genre) @@ plainto_tsquery('simple', $2) OR $2 = '')
+		ORDER BY %s %s, id ASC
+		LIMIT $3 OFFSET $4`,
+		filters.sortColumn(), filters.sortDirection())
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	rows, err := m.DB.QueryContext(ctx, query, name, genre, filters.limit(), filters.offset())
+	if err != nil {
+		return nil, nil, Metadata{}, err
+	}
+	defer rows.Close()
+
+	totalRecords := 0
+	games := []*Game{}
+	gameList := make(map[string]string)
+
+	for rows.Next() {
+		var game Game
+
+		err = rows.Scan(
+			&totalRecords,
+			&game.Id,
+			&game.Name,
+			&game.Genre,
+			&game.PublisherId,
+			&game.Version,
+		)
+		if err != nil {
+			return nil, nil, Metadata{}, err
+		}
+		gameList[game.Name] = game.Genre
+		games = append(games, &game)
+	}
+	if err = rows.Err(); err != nil {
+		return nil, nil, Metadata{}, err
+	}
+	metadata := calculateMetadata(totalRecords, filters.Page, filters.PageSize)
+
+	return games, gameList, metadata, nil
 }
 
 // Get all games from db
@@ -68,5 +118,6 @@ func (m *GameModel) GetAll() ([]*Game, map[string]string, error) {
 	if err = rows.Err(); err != nil {
 		return nil, nil, err
 	}
+
 	return games, gameList, nil
 }
